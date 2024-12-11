0b797a0f19d68b8e26c84475972ea2cda6f15ae9
Sorena Paydar <sorenapaydar81@gmail.com>
2024-12-11 16:33:19 +0330

    feat: user entity

---
+++ src/main/java/org/Smart/ExpenseSplitter/entity/UserEntity.java
@@ -0,0 +1,27 @@
+package org.Smart.ExpenseSplitter.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.UUID;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}

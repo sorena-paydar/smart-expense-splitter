09532a9031e71234740c1c9f4e9a275e454a2035
Sorena Paydar <sorena.paydar@digikala.com>
2024-12-11 10:35:41 +0330

    init

---
+++ src/main/java/org/example/smartexpensesplitter/SmartExpenseSplitterApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartexpensesplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpenseSplitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/smartexpensesplitter/SmartExpenseSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartexpensesplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpenseSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

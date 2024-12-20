6784d2bbd0fac1d858bd5c5ca957b06f0cfccc1c
Sorena Paydar <sorenapaydar81@gmail.com>
2024-12-20 21:37:36 +0330

    feat: include doc for the user repo

--- src/main/java/org/Smart/ExpenseSplitter/repository/UserRepository.java
+++ src/main/java/org/Smart/ExpenseSplitter/repository/UserRepository.java
@@ -6,10 +6,26 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface for managing {@link UserEntity} entities.
+ * Provides methods for common operations like finding users by username or email.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    /**
+     * Find a user by their username.
+     *
+     * @param username the username of the user to find
+     * @return an Optional containing the found UserEntity, or empty if no user is found
+     */
     Optional<UserEntity> findByUsername(String username);
 
+    /**
+     * Find a user by their email address.
+     *
+     * @param email the email of the user to find
+     * @return an Optional containing the found UserEntity, or empty if no user is found
+     */
     Optional<UserEntity> findByEmail(String email);
 }

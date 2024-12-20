767ddcd61ad4ea2a2c091bffca702aa4e8e28ef6
Sorena Paydar <sorenapaydar81@gmail.com>
2024-12-20 15:25:50 +0330

    feat: user not found exception

---
+++ src/main/java/org/Smart/ExpenseSplitter/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package org.Smart.ExpenseSplitter.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

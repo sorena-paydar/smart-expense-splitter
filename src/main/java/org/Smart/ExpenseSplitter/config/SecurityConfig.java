76ac7eab0c2b1547b7f7b70961c20de02b6243da
Sorena Paydar <sorenapaydar81@gmail.com>
2024-12-21 02:32:48 +0330

    feat: enable method-level security annotations

--- src/main/java/org/Smart/ExpenseSplitter/config/SecurityConfig.java
+++ src/main/java/org/Smart/ExpenseSplitter/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true) // Enable method-level security annotations
 class SecurityConfig {
 
     private final JwtService jwtTokenProvider;

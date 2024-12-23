c94e84cc6392eae543a827a3eb93dbadfd691a68
Sorena Paydar <sorenapaydar81@gmail.com>
2024-12-23 20:53:25 +0330

    fix: default swagger inputs for auth request DTO

--- src/main/java/org/Smart/ExpenseSplitter/dto/auth/AuthRequestDTO.java
+++ src/main/java/org/Smart/ExpenseSplitter/dto/auth/AuthRequestDTO.java
@@ -1,11 +1,18 @@
 package org.Smart.ExpenseSplitter.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class AuthRequestDTO {
+
+    @Schema(description = "Username for login", example = "test")
     private String username;
+
+    @Schema(description = "Email address for login", example = "test@gmail.com")
     private String email;
+
+    @Schema(description = "User's password", example = "1234")
     private String password;
 }
 
